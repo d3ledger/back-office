936325e158a158e1fca4b1cb9f8cd0cc53afc7fd
Vyacheslav Bikbaev <la.sintez@gmail.com>
2018-04-24 14:05:56 +0300

    feat: balance page stub + element ui
    Signed-off-by: Vyacheslav Bikbaev <la.sintez@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'normalize.css'
+import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/en'
 import App from './App'
 import router from './router'
+
+// TODO: import only necessary components
+Vue.use(ElementUI, { locale })
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import BalancePage from '@/components/BalancePage'
+import SettlementsPage from '@/components/SettlementsPage'
 
 Vue.use(Router)
 
@@ -8,8 +10,23 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: Home,
+      children: [
+        {
+          path: '',
+          name: 'balance-page',
+          component: BalancePage
+        },
+        {
+          path: '/settlements',
+          name: 'settlements-page',
+          component: SettlementsPage
+        }
+      ]
+    },
+    {
+      path: '*',
+      redirect: '/balance'
     }
   ]
 })
