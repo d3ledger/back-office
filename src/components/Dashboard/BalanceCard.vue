0c0275cfbbe9dbb4eef384438ed30ca51dfbc433
Vyacheslav Bikbaev <la.sintez@gmail.com>
2018-04-30 13:23:47 +0300

    feat: dashboard

---
+++ src/components/Dashboard/BalanceCard.vue
@@ -0,0 +1,53 @@
+<template>
+  <el-card>
+    <el-table
+      :data="wallets"
+      size="small"
+    >
+      <el-table-column
+        label="Name"
+        min-width="90px"
+      >
+        <template slot-scope="scope">
+          {{ scope.row.name }} <span>({{ scope.row.short }})</span>
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="Amount"
+        min-width="70px"
+      >
+        <template slot-scope="scope">
+          {{ Number(scope.row.amount).toFixed(scope.row.precision) }}
+        </template>
+      </el-table-column>
+      <el-table-column
+        width="193px"
+        fixed="right"
+      >
+        <template slot-scope="scope">
+          <el-button size="small">Deposit</el-button>
+          <el-button size="small">Withdraw</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </el-card>
+</template>
+
+<script>
+export default {
+  name: 'balance-card',
+  props: {
+    wallets: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
-import BalancePage from '@/components/BalancePage'
-import SettlementsPage from '@/components/SettlementsPage'
+import Dashboard from '@/components/Dashboard/Dashboard'
 
 Vue.use(Router)
 
@@ -14,13 +13,8 @@ export default new Router({
       children: [
         {
           path: '',
-          name: 'balance-page',
-          component: BalancePage
-        },
-        {
-          path: '/settlements',
-          name: 'settlements-page',
-          component: SettlementsPage
+          name: 'dashboard',
+          component: Dashboard
         }
       ]
     },
