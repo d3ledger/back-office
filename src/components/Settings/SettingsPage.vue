fe2340203aa60b29f04cc433226f986c21a87d9c
Vyacheslav Bikbaev <la.sintez@gmail.com>
2018-05-01 15:37:40 +0300

    feat: wallet add form

--- src/components/Settings/SettingsPage.vue
+++ src/components/Settings/SettingsPage.vue
@@ -4,14 +4,37 @@
       <wallets-card
         title="ETH wallets"
         :wallets="eth"
+        v-on:add-wallet="newWallet.type = 'ETH'; newWalletForm = true"
       />
     </el-col>
     <el-col :span="12">
       <wallets-card
         title="Waves wallets"
         :wallets="wvs"
+        v-on:add-wallet="newWallet.type = 'ETH'; newWalletForm = true"
       />
     </el-col>
+    <el-dialog
+      title="Add a wallet"
+      :visible.sync="newWalletForm"
+      width="500px"
+    >
+      <el-form
+        label-width="4rem"
+      >
+        <el-form-item label="Name:" prop="amount">
+          <el-input name="amount" v-model="newWallet.name">
+            <template slot="append">{{ newWallet.type }}</template>
+          </el-input>
+        </el-form-item>
+        <el-form-item label="Address:" prop="to">
+          <el-input name="address" v-model="newWallet.address" />
+        </el-form-item>
+        <el-form-item style="margin-bottom: 0;">
+          <el-button type="primary">Add Wallet</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </el-row>
 </template>
 
@@ -25,6 +48,12 @@ export default {
   },
   data () {
     return {
+      newWalletForm: false,
+      newWallet: {
+        name: null,
+        address: null,
+        type: null
+      },
       eth: [
         {
           name: 'Work wallet',
