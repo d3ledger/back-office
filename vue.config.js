abede2b3c4cf40415d41d9dc41c827772c2c9093
Keiji Yahata <yahata@soramitsu.co.jp>
2018-06-14 17:17:00 +0900

    test: coverage for unit tests

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,27 @@
 const path = require('path')
 
+const rules = []
+
+if (process.env.NODE_ENV === 'test') {
+  rules.push({
+    test: /\.(js|ts)$/,
+    include: path.resolve('src'),
+    loader: 'istanbul-instrumenter-loader',
+    query: {
+      esModules: true
+    }
+  })
+}
+
 module.exports = {
   configureWebpack: {
     resolve: {
       alias: {
         'util': path.resolve(__dirname, 'src/util')
       }
+    },
+    module: {
+      rules: rules
     }
   }
 }
