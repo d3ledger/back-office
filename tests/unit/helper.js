cd9ce5a6fc0ad38d27dfe5bd3eca221f5f627577
Keiji Yahata <yahata@soramitsu.co.jp>
2018-06-14 15:38:20 +0900

    test: Account's actions

--- tests/unit/helper.js
+++ tests/unit/helper.js
@@ -8,15 +8,7 @@ export default {
   randomAccountId: () => randomHex(5) + '@' + randomHex(5),
   randomAssetId: () => randomHex(5) + '#' + randomHex(5),
   randomNodeIp: () => 'localhost:' + _.random(0, 65535),
-  randomPublicKey: () => randomHex(16),
-  randomPrivateKey: () => randomHex(16),
+  randomPrivateKey: () => randomHex(64),
   randomObject: () => ({ [randomHex(5)]: randomHex(5) }),
-  randomAmount: () => String(Math.random()).substr(0, _.random(5, 10)),
-  randomAmountRng: (obj) => {
-    obj = obj || {}
-    obj.max = obj.max || 100000
-    obj.min = obj.min || 0
-    return _.random(obj.min, obj.max)
-  },
-  randomArrayElement: (arr) => _.sample(arr)
+  randomAmount: () => String(Math.random()).substr(0, _.random(5, 10))
 }
