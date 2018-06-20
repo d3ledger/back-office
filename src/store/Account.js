dc1833c9f4578a2676b1a64edf10ae74dfd4ff0f
Keiji Yahata <yahata@soramitsu.co.jp>
2018-06-20 17:05:13 +0900

    feat: include settlement data into tx if it is a part of a settlement

--- src/store/Account.js
+++ src/store/Account.js
@@ -60,6 +60,92 @@ function initialState () {
   }
 }
 
+// TODO: check if transferAsset is a part of a settlement by meta info.
+function getSettlementOfTransaction (transferAsset) {
+  if (transferAsset.description !== 'PART_OF_DUMMY_SETTLEMENT') {
+    return null
+  }
+
+  return {
+    'id': 1,
+    'from': 'you',
+    'offer_amount': 0.796463,
+    'offer_asset': 'WVS',
+    'to': 'yuriy@ru',
+    'request_amount': 0.26483,
+    'request_asset': 'ETH',
+    'date': '2018-03-24T00:19:35Z',
+    'message': 'Hello. This is a settlement.',
+    'status': 'accepted'
+  }
+}
+
+function transformTransactions (transactions) {
+  if (!transactions) return []
+
+  const transformed = []
+
+  transactions.forEach(t => {
+    const { commandsList, createdTime } = t.payload
+
+    commandsList.forEach(c => {
+      if (!c.transferAsset) return
+
+      const {
+        amount,
+        destAccountId,
+        srcAccountId,
+        description
+      } = c.transferAsset
+
+      const tx = {
+        from: srcAccountId === state.accountId ? 'you' : srcAccountId,
+        to: destAccountId === state.accountId ? 'you' : destAccountId,
+        amount: amountToString(amount),
+        date: createdTime,
+        message: description,
+        // TODO: set appropreate tx status ('accepted', 'rejected', 'canceled')
+        status: 'accepted'
+      }
+
+      const settlement = getSettlementOfTransaction(c.transferAsset)
+
+      if (settlement) tx.settlement = settlement
+
+      transformed.push(tx)
+    })
+  })
+
+  /*
+   * As actions.getAccountTransactions() does, we fetch account's txs
+   * by multiple getAccount*Asset*Transactions calls.
+   *
+   * Also, getAccount*Asset*Transactions returns txs each of which includes
+   * one or more command(s), which possibly includes also commands issued
+   * against different asset.
+   *
+   * Therefore, when merging transactions for multiple assets, duplication
+   * possibly occurs.
+   * e.g.
+   *    accountAssetTransactions_of_asset_A = [
+   *      { commands: [command_for_asset_A_1, command_for_asset_B_1] },
+   *      { commands: [command_for_asset_A_2] }
+   *    ]
+   *    accountAssetTransactions_of_asset_B = [
+   *      { commands: [command_for_asset_A_1, command_for_asset_B_1] }
+   *    ]
+   *    // -> command_for_asset_A_1 and B_1 duplicates!
+   *
+   * To avoid it, we uniq the transactions.
+   */
+  return _(transformed)
+    .chain()
+    .uniqWith(_.isEqual)
+    .sortBy('date')
+    .reverse()
+    .value()
+}
+
 const state = initialState()
 
 const getters = {
