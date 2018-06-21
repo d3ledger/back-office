e01e9218497edd3ccf9a46b80e8843f3f7161505
Keiji Yahata <yahata@soramitsu.co.jp>
2018-06-21 17:03:25 +0900

    feat: create a settlement

--- src/components/Settlements/SettlementsPage.vue
+++ src/components/Settlements/SettlementsPage.vue
@@ -25,10 +25,52 @@
       <el-row>
         <el-col :xs="24" :md="{ span: 20, offset: 2 }" :lg="{ span: 18, offset: 3 }" :xl="{ span: 16, offset: 4 }">
           <router-view />
-        </el-col>
-      </el-row>
-    </el-main>
-  </el-container>
+        </el-card>
+      </el-col>
+    </el-row>
+
+     <el-dialog
+      title="New Settlement"
+      :visible.sync="newSettlementFormVisible"
+      width="500px"
+    >
+      <el-form label-width="4rem">
+        <el-form-item label="I offer:" prop="amount">
+          <el-input name="amount" v-model="newSettlementForm.request_amount">
+            <el-select
+              v-model="newSettlementForm.request_asset"
+              slot="append"
+              placeholder="asset"
+              style="width: 100px"
+            >
+              <el-option label="ETH" value="eth">ETH</el-option>
+              <el-option label="WVS" value="wvs">WVS</el-option>
+            </el-select>
+          </el-input>
+        </el-form-item>
+        <el-form-item label="To">
+          <el-input v-model="newSettlementForm.to" placeholder="account id" />
+        </el-form-item>
+        <el-form-item label="For:" prop="amount">
+          <el-input name="amount" v-model="newSettlementForm.offer_amount">
+            <el-select
+              v-model="newSettlementForm.offer_asset"
+              slot="append"
+              placeholder="asset"
+              style="width: 100px"
+            >
+              <el-option label="ETH" value="eth">ETH</el-option>
+              <el-option label="WVS" value="wvs">WVS</el-option>
+            </el-select>
+          </el-input>
+        </el-form-item>
+        <el-form-item style="margin-bottom: 0;">
+          <el-button type="primary" @click="onCreateSettlement">Create settlement</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
+
+  </section>
 </template>
 
 <script>
@@ -36,13 +78,44 @@ import { mapActions } from 'vuex'
 
 export default {
   data () {
-    return {}
+    return {
+      newSettlementFormVisible: false,
+      newSettlementForm: {
+        to: null,
+        request_amount: null,
+        request_asset: null,
+        offer_amount: null,
+        offer_asset: null
+      }
+    }
   },
 
   methods: {
-    ...mapActions([
-      'openExchangeDialog'
-    ])
+    onCreateSettlement () {
+      const s = this.newSettlementForm
+
+      this.$store.dispatch('createSettlement', {
+        to: s.to,
+        offerAssetId: s.offer_asset,
+        offerAmount: s.offer_amount,
+        requestAssetId: s.request_asset,
+        requestAmount: s.request_amount
+      })
+        .then(() => {
+          this.$message('New settlement has successfully been created')
+        })
+        .catch(err => {
+          console.error(err)
+          this.$message('Failed to create new settlement')
+        })
+        .finally(() => {
+          Object.assign(
+            this.$data.newSettlementForm,
+            this.$options.data().newSettlementForm
+          )
+          this.newSettlementFormVisible = false
+        })
+    }
   }
 }
 </script>
