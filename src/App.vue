1f01b70789b15a475682889eb6fea4859cba66d6
Vyacheslav Bikbaev <la.sintez@gmail.com>
2018-06-21 20:15:20 +0300

    feat: blue reports, refactor
    Signed-off-by: Vyacheslav Bikbaev <la.sintez@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -76,4 +76,62 @@ a:hover {
 .el-tooltip__popper, .el-select-dropdown__item {
   font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
 }
+
+.form-item-text {
+  font-size: 14px;
+  opacity: 0.7;
+}
+
+.form-item-text-amount {
+  font-weight: 500;
+}
+
+.blue-form-wrapper {
+  background: #669dd5;
+  color: white;
+  padding: 20px 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.blue-form-wrapper .el-form-item__label {
+  color: white;
+}
+
+.blue-form-wrapper .el-input__inner, .blue-form-wrapper .el-textarea__inner {
+  border-color: rgba(255, 255, 255, 0.5);
+  background: #669dd5;
+  color: white;
+}
+
+.blue-form-wrapper .el-input__inner:focus, .blue-form-wrapper .el-textarea__inner:focus {
+  border-color: white !important;
+  background-color: rgba(255, 255, 255, 0.08);
+}
+
+.blue-form-wrapper .el-input--suffix {
+  color: black;
+}
+
+.blue-form-wrapper .el-button {
+  color: white;
+  background: #041820;
+  border: 1px solid #041820;
+  width: 100%;
+}
+
+.blue-form-wrapper .el-range-input {
+  color: white;
+  background: #669dd5;
+}
+
+.blue-form-wrapper .el-range-separator, .blue-form-wrapper .el-input__icon  .el-range__close-icon {
+  color: white;
+}
+
+.blue-form-wrapper .el-range-input {
+  width: 50% !important;
+}
 </style>
