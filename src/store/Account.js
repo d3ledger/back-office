cc706d897c359c17ae98c834397452d14b9d91a2
Vyacheslav Bikbaev <la.sintez@gmail.com>
2018-06-28 19:20:31 +0300

    feat: storing state of approval window.

--- src/store/Account.js
+++ src/store/Account.js
@@ -80,6 +80,8 @@ const getters = {
       .filter(x => x.status === 'waiting')
   },
 
+  // todo: 'you' -> accountId :)
+
   incomingSettlements () {
     return getters.waitingSettlements().filter(x => x.to === 'you')
   },
