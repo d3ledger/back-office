0a5a14c487aa57ab30a7d52dd13158e4291dc476
Pobepto <roobertdantes@gmail.com>
2018-07-06 13:14:38 +0300

    feat: finish implementation of charts and list of crypto

--- src/components/Dashboard/DashboardChart.vue
+++ src/components/Dashboard/DashboardChart.vue
@@ -3,30 +3,50 @@
     <div slot="header">
       <el-row>
         <el-col :span="18">
-          <p>Cardano (ADA)</p>
+          <p>{{ portfolioChart.crypto }}</p>
         </el-col>
         <el-col :span="6">
-          <p class="chart_time-filter">1H</p>
-          <p class="chart_time-filter">1D</p>
-          <p class="chart_time-filter">1W</p>
-          <p class="chart_time-filter selected">1M</p>
-          <p class="chart_time-filter">1Y</p>
-          <p class="chart_time-filter">ALL</p>
+          <p
+            v-for="(value, index) in daysLabels"
+            :key="index"
+            :class="[portfolioChart.filter !== value ? 'chart_time-filter' : 'chart_time-filter selected']"
+            @click="selectLabel(value)">
+            {{ value }}
+          </p>
         </el-col>
       </el-row>
     </div>
     <div>
-      <line-chart/>
+      <line-chart :data="portfolioChart.data" :filter="portfolioChart.filter"/>
     </div>
   </el-card>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import LineChart from '@/components/Dashboard/Charts/LineChart'
 
 export default {
   components: {
     LineChart
+  },
+  data () {
+    return {
+      daysLabels: ['1H', '1D', '1W', '1M', '1Y', 'ALL']
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'portfolioChart'
+    ])
+  },
+  mounted () {
+    this.$store.dispatch('getPriceByFilter', this.portfolioChart)
+  },
+  methods: {
+    selectLabel (label) {
+      this.$store.dispatch('getPriceByFilter', { filter: label })
+    }
   }
 }
 </script>
@@ -37,7 +57,12 @@ export default {
   opacity: 0.3;
   margin-right: 10px;
 }
+.chart_time-filter {
+  cursor: pointer;
+}
 .chart_time-filter.selected {
   opacity: 1;
+  font-weight: 600;
+  border-bottom: 2px solid #000000;
 }
 </style>
