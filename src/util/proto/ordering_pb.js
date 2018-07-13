cfa484ce968cc1d30da08d08044f42ca3560348c
Vyacheslav Bikbaev <la.sintez@gmail.com>
2018-07-13 15:37:21 +0300

    feat: added proto-files, generated by protoc

---
+++ src/util/proto/ordering_pb.js
@@ -0,0 +1,17 @@
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+
+var jspb = require('google-protobuf');
+var goog = jspb;
+var global = Function('return this')();
+
+var block_pb = require('./block_pb.js');
+var proposal_pb = require('./proposal_pb.js');
+var google_protobuf_empty_pb = require('google-protobuf/google/protobuf/empty_pb.js');
+goog.object.extend(exports, proto.iroha.ordering.proto);
