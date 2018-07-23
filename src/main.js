ab8fcdfff032ab525b5e079175e205879fce5a04
Pobepto <roobertdantes@gmail.com>
2018-07-23 13:08:26 +0300

    chore: import per icon

--- src/main.js
+++ src/main.js
@@ -6,7 +6,19 @@ import router from './router'
 import store from './store'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCog, faChartLine, faWallet, faSignOutAlt, faFileInvoice, faExchangeAlt, faAngleDoubleDown, faAngleDoubleUp, faArrowRight, faDownload, faUpload } from '@fortawesome/free-solid-svg-icons'
+
+import { faCog } from '@fortawesome/free-solid-svg-icons/faCog'
+import { faChartLine } from '@fortawesome/free-solid-svg-icons/faChartLine'
+import { faWallet } from '@fortawesome/free-solid-svg-icons/faWallet'
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons/faSignOutAlt'
+import { faFileInvoice } from '@fortawesome/free-solid-svg-icons/faFileInvoice'
+import { faExchangeAlt } from '@fortawesome/free-solid-svg-icons/faExchangeAlt'
+import { faAngleDoubleDown } from '@fortawesome/free-solid-svg-icons/faAngleDoubleDown'
+import { faAngleDoubleUp } from '@fortawesome/free-solid-svg-icons/faAngleDoubleUp'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons/faArrowRight'
+import { faDownload } from '@fortawesome/free-solid-svg-icons/faDownload'
+import { faUpload } from '@fortawesome/free-solid-svg-icons/faUpload'
+
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import ElementUI from 'element-ui'
