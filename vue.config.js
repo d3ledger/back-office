a62071184d53f5d20717fb5a9bf0a8163550997e
Pobepto <roobertdantes@gmail.com>
2018-07-25 15:23:26 +0300

    fix: remove hardCache

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const webpack = require('webpack')
-const HardSourceWebpackPlugin = require('hard-source-webpack-plugin')
 
 const rules = []
 
@@ -21,7 +20,6 @@ module.exports = {
       .set('@util', path.resolve(__dirname, 'src/util'))
       .set('@router', path.resolve(__dirname, 'src/router.js'))
     config.plugin('IgnorePlugin').use(webpack.IgnorePlugin, [/^\.\/locale$/, /moment$/])
-    config.plugin('HardSource').use(HardSourceWebpackPlugin)
     config.plugin('html').tap(args => {
       args[0].chunksSortMode = 'none'
       return args
