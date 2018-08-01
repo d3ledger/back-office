a221f86acac0b615c1e2e20eacd13b6d6ebb53f4
Vyacheslav Bikbaev <la.sintez@gmail.com>
2018-08-01 12:16:10 +0300

    chore: transfered proto files

--- src/util/iroha-helpers/proto/endpoint_pb_service.js
+++ src/util/iroha-helpers/proto/endpoint_pb_service.js
@@ -1,65 +1,65 @@
 // package: iroha.protocol
 // file: endpoint.proto
 
-var endpoint_pb = require("./endpoint_pb");
-var transaction_pb = require("./transaction_pb");
-var queries_pb = require("./queries_pb");
-var qry_responses_pb = require("./qry_responses_pb");
-var google_protobuf_empty_pb = require("google-protobuf/google/protobuf/empty_pb");
-var grpc = require("grpc-web-client").grpc;
+var endpoint_pb = require('./endpoint_pb')
+var transaction_pb = require('./transaction_pb')
+var queries_pb = require('./queries_pb')
+var qry_responses_pb = require('./qry_responses_pb')
+var google_protobuf_empty_pb = require('google-protobuf/google/protobuf/empty_pb')
+var grpc = require('grpc-web-client').grpc
 
 var CommandService = (function () {
-  function CommandService() {}
-  CommandService.serviceName = "iroha.protocol.CommandService";
-  return CommandService;
-}());
+  function CommandService () {}
+  CommandService.serviceName = 'iroha.protocol.CommandService'
+  return CommandService
+}())
 
 CommandService.Torii = {
-  methodName: "Torii",
+  methodName: 'Torii',
   service: CommandService,
   requestStream: false,
   responseStream: false,
   requestType: transaction_pb.Transaction,
   responseType: google_protobuf_empty_pb.Empty
-};
+}
 
 CommandService.ListTorii = {
-  methodName: "ListTorii",
+  methodName: 'ListTorii',
   service: CommandService,
   requestStream: false,
   responseStream: false,
   requestType: endpoint_pb.TxList,
   responseType: google_protobuf_empty_pb.Empty
-};
+}
 
 CommandService.Status = {
-  methodName: "Status",
+  methodName: 'Status',
   service: CommandService,
   requestStream: false,
   responseStream: false,
   requestType: endpoint_pb.TxStatusRequest,
   responseType: endpoint_pb.ToriiResponse
-};
+}
 
 CommandService.StatusStream = {
-  methodName: "StatusStream",
+  methodName: 'StatusStream',
   service: CommandService,
   requestStream: false,
   responseStream: true,
   requestType: endpoint_pb.TxStatusRequest,
   responseType: endpoint_pb.ToriiResponse
-};
-
-exports.CommandService = CommandService;
-
-function CommandServiceClient(serviceHost, options) {
-  this.serviceHost = serviceHost;
-  this.options = options || {};
 }
 
-CommandServiceClient.prototype.torii = function torii(requestMessage, metadata, callback) {
+exports.CommandService = CommandService
+
+function CommandServiceClient (serviceHost, options) {
+  this.serviceHost = serviceHost
+  this.options = options || {}
+}
+
+CommandServiceClient.prototype.torii = function torii (requestMessage, metadata, callback) {
   if (arguments.length === 2) {
-    callback = arguments[1];
+    callback = arguments[1]
   }
   grpc.unary(CommandService.Torii, {
     request: requestMessage,
@@ -69,18 +69,18 @@ CommandServiceClient.prototype.torii = function torii(requestMessage, metadata, 
     onEnd: function (response) {
       if (callback) {
         if (response.status !== grpc.Code.OK) {
-          callback(Object.assign(new Error(response.statusMessage), { code: response.status, metadata: response.trailers }), null);
+          callback(Object.assign(new Error(response.statusMessage), { code: response.status, metadata: response.trailers }), null)
         } else {
-          callback(null, response.message);
+          callback(null, response.message)
         }
       }
     }
-  });
-};
+  })
+}
 
-CommandServiceClient.prototype.listTorii = function listTorii(requestMessage, metadata, callback) {
+CommandServiceClient.prototype.listTorii = function listTorii (requestMessage, metadata, callback) {
   if (arguments.length === 2) {
-    callback = arguments[1];
+    callback = arguments[1]
   }
   grpc.unary(CommandService.ListTorii, {
     request: requestMessage,
@@ -90,18 +90,18 @@ CommandServiceClient.prototype.listTorii = function listTorii(requestMessage, me
     onEnd: function (response) {
       if (callback) {
         if (response.status !== grpc.Code.OK) {
-          callback(Object.assign(new Error(response.statusMessage), { code: response.status, metadata: response.trailers }), null);
+          callback(Object.assign(new Error(response.statusMessage), { code: response.status, metadata: response.trailers }), null)
         } else {
-          callback(null, response.message);
+          callback(null, response.message)
         }
       }
     }
-  });
-};
+  })
+}
 
-CommandServiceClient.prototype.status = function status(requestMessage, metadata, callback) {
+CommandServiceClient.prototype.status = function status (requestMessage, metadata, callback) {
   if (arguments.length === 2) {
-    callback = arguments[1];
+    callback = arguments[1]
   }
   grpc.unary(CommandService.Status, {
     request: requestMessage,
@@ -111,21 +111,21 @@ CommandServiceClient.prototype.status = function status(requestMessage, metadata
     onEnd: function (response) {
       if (callback) {
         if (response.status !== grpc.Code.OK) {
-          callback(Object.assign(new Error(response.statusMessage), { code: response.status, metadata: response.trailers }), null);
+          callback(Object.assign(new Error(response.statusMessage), { code: response.status, metadata: response.trailers }), null)
         } else {
-          callback(null, response.message);
+          callback(null, response.message)
         }
       }
     }
-  });
-};
+  })
+}
 
-CommandServiceClient.prototype.statusStream = function statusStream(requestMessage, metadata) {
+CommandServiceClient.prototype.statusStream = function statusStream (requestMessage, metadata) {
   var listeners = {
     data: [],
     end: [],
     status: []
-  };
+  }
   var client = grpc.invoke(CommandService.StatusStream, {
     request: requestMessage,
     host: this.serviceHost,
@@ -133,67 +133,67 @@ CommandServiceClient.prototype.statusStream = function statusStream(requestMessa
     transport: this.options.transport,
     onMessage: function (responseMessage) {
       listeners.data.forEach(function (handler) {
-        handler(responseMessage);
-      });
+        handler(responseMessage)
+      })
     },
     onEnd: function (status, statusMessage, trailers) {
       listeners.end.forEach(function (handler) {
-        handler();
-      });
+        handler()
+      })
       listeners.status.forEach(function (handler) {
-        handler({ code: status, details: statusMessage, metadata: trailers });
-      });
-      listeners = null;
+        handler({ code: status, details: statusMessage, metadata: trailers })
+      })
+      listeners = null
     }
-  });
+  })
   return {
     on: function (type, handler) {
-      listeners[type].push(handler);
-      return this;
+      listeners[type].push(handler)
+      return this
     },
     cancel: function () {
-      listeners = null;
-      client.close();
+      listeners = null
+      client.close()
     }
-  };
-};
+  }
+}
 
-exports.CommandServiceClient = CommandServiceClient;
+exports.CommandServiceClient = CommandServiceClient
 
 var QueryService = (function () {
-  function QueryService() {}
-  QueryService.serviceName = "iroha.protocol.QueryService";
-  return QueryService;
-}());
+  function QueryService () {}
+  QueryService.serviceName = 'iroha.protocol.QueryService'
+  return QueryService
+}())
 
 QueryService.Find = {
-  methodName: "Find",
+  methodName: 'Find',
   service: QueryService,
   requestStream: false,
   responseStream: false,
   requestType: queries_pb.Query,
   responseType: qry_responses_pb.QueryResponse
-};
+}
 
 QueryService.FetchCommits = {
-  methodName: "FetchCommits",
+  methodName: 'FetchCommits',
   service: QueryService,
   requestStream: false,
   responseStream: true,
   requestType: queries_pb.BlocksQuery,
   responseType: qry_responses_pb.BlockQueryResponse
-};
-
-exports.QueryService = QueryService;
-
-function QueryServiceClient(serviceHost, options) {
-  this.serviceHost = serviceHost;
-  this.options = options || {};
 }
 
-QueryServiceClient.prototype.find = function find(requestMessage, metadata, callback) {
+exports.QueryService = QueryService
+
+function QueryServiceClient (serviceHost, options) {
+  this.serviceHost = serviceHost
+  this.options = options || {}
+}
+
+QueryServiceClient.prototype.find = function find (requestMessage, metadata, callback) {
   if (arguments.length === 2) {
-    callback = arguments[1];
+    callback = arguments[1]
   }
   grpc.unary(QueryService.Find, {
     request: requestMessage,
@@ -203,21 +203,21 @@ QueryServiceClient.prototype.find = function find(requestMessage, metadata, call
     onEnd: function (response) {
       if (callback) {
         if (response.status !== grpc.Code.OK) {
-          callback(Object.assign(new Error(response.statusMessage), { code: response.status, metadata: response.trailers }), null);
+          callback(Object.assign(new Error(response.statusMessage), { code: response.status, metadata: response.trailers }), null)
         } else {
-          callback(null, response.message);
+          callback(null, response.message)
         }
       }
     }
-  });
-};
+  })
+}
 
-QueryServiceClient.prototype.fetchCommits = function fetchCommits(requestMessage, metadata) {
+QueryServiceClient.prototype.fetchCommits = function fetchCommits (requestMessage, metadata) {
   var listeners = {
     data: [],
     end: [],
     status: []
-  };
+  }
   var client = grpc.invoke(QueryService.FetchCommits, {
     request: requestMessage,
     host: this.serviceHost,
@@ -225,30 +225,29 @@ QueryServiceClient.prototype.fetchCommits = function fetchCommits(requestMessage
     transport: this.options.transport,
     onMessage: function (responseMessage) {
       listeners.data.forEach(function (handler) {
-        handler(responseMessage);
-      });
+        handler(responseMessage)
+      })
     },
     onEnd: function (status, statusMessage, trailers) {
       listeners.end.forEach(function (handler) {
-        handler();
-      });
+        handler()
+      })
       listeners.status.forEach(function (handler) {
-        handler({ code: status, details: statusMessage, metadata: trailers });
-      });
-      listeners = null;
+        handler({ code: status, details: statusMessage, metadata: trailers })
+      })
+      listeners = null
     }
-  });
+  })
   return {
     on: function (type, handler) {
-      listeners[type].push(handler);
-      return this;
+      listeners[type].push(handler)
+      return this
     },
     cancel: function () {
-      listeners = null;
-      client.close();
+      listeners = null
+      client.close()
     }
-  };
-};
+  }
+}
 
-exports.QueryServiceClient = QueryServiceClient;
-
+exports.QueryServiceClient = QueryServiceClient
