359912e714e2fe2edad549eb268905666e966633
Pobepto <roobertdantes@gmail.com>
2018-08-06 13:56:11 +0300

    test: added first e2e tests, removed nightwatch

---
+++ tests/e2e/integration/settings.spec.js
@@ -0,0 +1,29 @@
+import path from 'path'
+import { URL } from '../config'
+
+const testKeyPath = path.resolve('test@notary.priv')
+
+describe('Test settings page', () => {
+  it('Make auth', () => {
+    cy.visit(URL)
+    cy.login(testKeyPath)
+  })
+
+  it('Go to settings page', () => {
+    cy.get('li.el-menu-item:nth-of-type(5)').click({ force: true })
+    cy.get('.el-card__header > div').contains('Settings')
+  })
+
+  it('Change settings', () => {
+    cy.get(':nth-child(1) > .el-col > .el-radio-group > :nth-child(2)').click().should(() => {
+      expect(localStorage.getItem('settings')).to.eq('{"view":{"fiat":"USD"}}')
+    })
+    cy.get(':nth-child(2) > .el-col > .el-radio-group > :nth-child(3)').click().should(() => {
+      expect(localStorage.getItem('settings')).to.eq('{"view":{"fiat":"USD","crypto":"XRP"}}')
+    })
+    cy.get('.el-input__inner').type('Europe/Dublin')
+    cy.contains('ul', 'Europe/Dublin').click().should(() => {
+      expect(localStorage.getItem('settings')).to.eq('{"view":{"fiat":"USD","crypto":"XRP","timezone":"Europe/Dublin"}}')
+    })
+  })
+})
