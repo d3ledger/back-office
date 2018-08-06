eef0fa0b946798665b1ec3fbc44825b2966be97e
Pobepto <roobertdantes@gmail.com>
2018-08-06 13:56:11 +0300

    test: added first e2e tests, removed nightwatch

--- tests/e2e/integration/auth.spec.js
+++ tests/e2e/integration/auth.spec.js
@@ -1,13 +1,16 @@
-const testKeyPath = 'test@d3.priv'
+import path from 'path'
+import { URL } from '../config'
+
+const testKeyPath = path.resolve('test@notary.priv')
 
 describe('Test login page', () => {
   it('Log in', () => {
-    cy.visit('/')
+    cy.visit(URL)
     cy.login(testKeyPath)
   })
 
   it('Log out', () => {
-    cy.get('.el-side-menu .el-menu-item:contains("Logout")').click({ force: true })
+    cy.get('li.el-menu-item:nth-of-type(6)').click({ force: true })
     cy.contains('Login').should('be.visible')
   })
 })
@@ -18,19 +21,18 @@ describe('Test register page', () => {
     cy.contains('Sign Up').should('be.visible')
   })
 
-  // Signing up should fail because the registration service doesn't work on CI now
-  it('Register new user - failure', () => {
-    cy.get('.el-input__inner[name="username"]').type('jasonstatham')
+  it('Register new user', () => {
+    cy.get('.el-input__inner').type('jasonstatham')
       .should('have.value', 'jasonstatham')
-    cy.get('.el-input__inner[name="newAddress"]').type('0x070f9d09370fd7ae3a583fc22a4e9f50ae1bdc78')
-      .should('have.value', '0x070f9d09370fd7ae3a583fc22a4e9f50ae1bdc78')
-    cy.get('.el-form-item__content > .el-button').contains('ADD').click()
-    cy.get('.el-form-item__content > .el-button.fullwidth').click()
-    cy.get('.el-message-box__title:contains("Sign up error")').should('be.visible')
+    cy.get('.el-form-item__content > .el-button').click()
+    cy.contains('Download').should('be.visible')
+    cy.contains('Confirm').should('be.disabled')
+    cy.get('.dialog-footer > .black').click()
+    cy.contains('Confirm').should('not.be.disabled')
   })
 
-  it.skip('Confirm should redirect to Log in', () => {
+  it('Confirm should redirect to Log in', () => {
     cy.contains('Confirm').click()
-    cy.url().should('be.eq', `${Cypress.config('baseUrl')}/#/login`)
+    cy.url().should('be.eq', `${URL}/#/login`)
   })
 })
--- tests/e2e/support/command.js
+++ tests/e2e/support/command.js
@@ -1,3 +1,5 @@
+import { IROHA_URL } from '../config'
+
 Cypress.Commands.add('upload_file', (fileName, selector) => {
   return cy.get(selector).then(subject => {
     return cy.fixture(fileName, 'base64')
@@ -13,42 +15,14 @@ Cypress.Commands.add('upload_file', (fileName, selector) => {
   })
 })
 
-Cypress.Commands.add('goToPage', (url, expect) => {
-  cy.wait(2000)
-  cy.get(`.el-side-menu .el-menu-item:contains("${expect}")`).click({ force: true })
-  cy.url().should('contain', url)
-})
-
 Cypress.Commands.add('login', (keyPath) => {
+  cy.get('form > div:nth-child(1) input')
+    .type('test@notary').should('have.value', 'test@notary')
   cy.upload_file(keyPath, 'input.el-upload__input')
+  cy.get('form > div:nth-child(2) input')
+    .should('have.value', '0f0ce16d2afbb8eca23c7d8c2724f0c257a800ee2bbd54688cec6b898e3f7e33')
   cy.get('form > div:nth-child(3) input')
-    .type(Cypress.env('IROHA')).should('have.value', Cypress.env('IROHA'))
-  cy.get('.el-scrollbar__view > :nth-child(1)').click()
+    .type(IROHA_URL).should('have.value', IROHA_URL)
   cy.get('.login-button-container > div > button').click()
-  cy.url().should('be.not.eq', `${Cypress.config('baseUrl')}/#/login`)
-})
-
-Cypress.Commands.add('setTimezone', (timezone) => {
-  cy.goToPage('settings', 'Settings')
-  cy.get('#timezone_select').should('be.visible').type(timezone)
-  cy.get('.el-select-dropdown .el-select-dropdown__list').contains(new RegExp(`^\\s*${timezone}\\s*$`)).click({ force: true }).should(() => {
-    const view = JSON.parse(localStorage.getItem('settings')).view
-
-    expect(view).to.have.property('timezone', timezone)
-  })
-})
-
-/*
- * In test environment, clicking a download button opens an alert instead of
- * downloading a file so Cypress can detect window:alert event to verify that
- * the correct file will be downloaded.
- */
-Cypress.Commands.add('shouldDownload', {
-  prevSubject: true
-}, (subject, expectedFilename) => {
-  const stub = cy.stub()
-
-  cy.on('window:alert', stub)
-  cy.wrap(subject).should(() => expect(stub.called).to.be.true)
-    .then(() => expect(stub.getCall(0)).to.be.calledWith(`downloading ${expectedFilename}`))
+  cy.contains('D3').should('be.visible')
 })
