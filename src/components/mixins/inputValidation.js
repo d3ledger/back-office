6efbc984ec2e733ea140a200e7ec2ba9305eeea6
Robert Sayakhov <roobertdantes@gmail.com>
2018-08-10 16:39:10 +0300

    Feature/d3 150 form validation (#27)
    * feat: added inputValidation mixin
    * feat: added validation to form components
    * feat: change error position
    * feat: added balance validation
    * feat: added rules for validator
    * feat: fields reset on close
    * fix: correct error labels position
    * feat: exchange modal validate fields correctly
    * fix: change pattern for nodeIp validation
    * fix: checking exchangeForm
    * fix: added prop for form-item
    * ffeat: remove http regex validation
    * feat: added validation for percission

--- src/components/mixins/inputValidation.js
+++ src/components/mixins/inputValidation.js
@@ -1,11 +1,5 @@
 import gt from 'lodash/fp/gt'
 import lte from 'lodash/fp/lte'
-import { derivePublicKey } from 'ed25519.js'
-
-const privateKey = {
-  pattern: /^[A-Za-z0-9]{64}$/,
-  message: 'Private key should match [A-Za-z0-9]{64}'
-}
 
 const set = {
   name: [
@@ -17,14 +11,16 @@ const set = {
     { pattern: /^[a-z_0-9]{1,32}@[a-z_0-9]{1,9}$/, message: 'Username should match [a-Z_0-9]{1,32}@[a-Z_0-9]{1,9}', trigger: 'change' }
   ],
   privateKey: [
-    { pattern: privateKey.pattern, message: privateKey.message, trigger: 'change' }
+    { required: true, message: 'Please input private key', trigger: 'change' },
+    { pattern: /^[A-Za-z0-9]{64}$/, message: 'Private key should match [A-Za-z0-9]{64}', trigger: 'change' }
   ],
   nodeIp: [
     { required: true, message: 'Please input node ip', trigger: 'change' },
     { pattern: /^([a-z0-9\-.]*)\.(([a-z]{2,4})|([0-9]{1,3}\.([0-9]{1,3})\.([0-9]{1,3})))|(:[0-9]{1,5})$/, message: 'Invalid IP', trigger: 'change' }
   ],
   walletAddress: [
-    { required: true, message: 'Please input wallet address', trigger: 'change' }
+    { required: true, message: 'Please input wallet address', trigger: 'change' },
+    { pattern: /^0x[a-fA-F0-9]{40}$/, message: 'Invalid wallet address', trigger: 'change' }
   ],
   tokensAmount: [
     { required: true, message: 'Please input amount', trigger: 'change' },
@@ -32,42 +28,16 @@ const set = {
   ]
 }
 
-set['privateKeyRequired'] = [
-  { required: true, message: 'Please input private key', trigger: 'change' },
-  ...set['privateKey']
-]
-
 const getPrecision = (v) => (v.split('.')[1] || []).length
 
-function checkBalance (maxValue, maxPrecision, asset) {
+function checkBalance (maxValue, maxPrecision) {
   return function validator (rule, value, callback, source, options) {
     const errors = []
-    if (!asset) errors.push('Please select asset')
-    else if (isNaN(Number(value))) errors.push('Invalid amount')
+    if (isNaN(Number(value))) errors.push('Invalid amount')
     else if (value !== null && gt(getPrecision(value))(maxPrecision)) errors.push(`Too big precision, maximum precision is ${maxPrecision}`)
     else if (value !== null && value.length === 0) errors.push('Please input amount')
-    else if (gt(Number(value))(Number(maxValue))) errors.push('Current amount is bigger than your available balance')
-    else if (lte(Number(value))(0)) errors.push('Current amount is smaller or equal to 0')
-    callback(errors)
-  }
-}
-
-function checkWallet (wallets) {
-  return function validator (rule, value, callback, source, options) {
-    const errors = []
-    const validateBTC = /^[123][a-km-zA-HJ-NP-Z1-9]{25,34}$/.test(value)
-    const validateETH = /^0x[a-fA-F0-9]{40}$/.test(value)
-    if (!validateBTC && !validateETH) errors.push('Invalid wallet address')
-    else if (wallets && wallets.includes(value)) errors.push('This wallet is already in the whitelist')
-    callback(errors)
-  }
-}
-
-function checkRepeatingPrivateKey (keys) {
-  return function validator (rule, value, callback, source, options) {
-    const errors = []
-    if (!!value && !privateKey.pattern.test(value)) errors.push(privateKey.message)
-    else if (keys.includes(derivePublicKey(Buffer.from(value, 'hex')).toString('hex'))) errors.push('Transaction is already signed with this key')
+    else if (gt(value)(maxValue)) errors.push('Current amount is bigger than your available balance')
+    else if (lte(value)(0)) errors.push('Current amount is smaller or equal to 0')
     callback(errors)
   }
 }
@@ -78,17 +48,8 @@ function generateRules (form) {
     const validationRule = form[key]
     if (validationRule.pattern === 'tokensAmount') {
       const tokensAmountRule = Object.assign([], set[validationRule.pattern])
-      tokensAmountRule.push({
-        validator: checkBalance(validationRule.amount, validationRule.precision, validationRule.asset)
-      })
+      tokensAmountRule.push({ validator: checkBalance(validationRule.amount, validationRule.precision) })
       rules[key] = tokensAmountRule
-    } else if (validationRule.pattern === 'walletAddress') {
-      const walletAddressRule = Object.assign([], set[validationRule])
-      walletAddressRule.push({ validator: checkWallet(validationRule.wallets) })
-      rules[key] = walletAddressRule
-    } else if (validationRule.pattern === 'repeatingPrivateKey') {
-      const privateKeyRule = [{ validator: checkRepeatingPrivateKey(validationRule.keys) }]
-      rules[key] = privateKeyRule
     } else {
       rules[key] = set[validationRule]
     }
