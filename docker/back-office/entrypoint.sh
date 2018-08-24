a2e5a5ea1bbfcc9f8f699f0b9cc91bc16f8054fc
Alexey Rodionov <rodionov@soramitsu.co.jp>
2018-08-24 08:07:11 +0300

    Add docker-compose file for e2e tests
    Signed-off-by: Alexey Rodionov <rodionov@soramitsu.co.jp>

---
+++ docker/back-office/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd /app
+yarn
+
+curl -s -o /dev/null -m 10 -I http://grpcwebproxy:8080
+sleep 10
+
+NODE_IP=http://grpcwebproxy:8080 DEBUG=iroha-util node scripts/setup.js
+yarn serve
+
+# curl -s -o /dev/null -m 30 -I http://localhost:8080
+
+
+# sleep 30
+# CYPRESS_baseUrl=http://localhost:8080 CYPRESS_IROHA=http://grpcwebproxy:8080 cypress run
