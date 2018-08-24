05e54ee009de888fed3340a22b483f7b525eb840
Alexey Rodionov <rodionov@soramitsu.co.jp>
2018-08-24 09:38:26 +0300

    fix scripts
    Signed-off-by: Alexey Rodionov <rodionov@soramitsu.co.jp>

--- docker/back-office/entrypoint.sh
+++ docker/back-office/entrypoint.sh
@@ -3,9 +3,9 @@ cd /app
 yarn
 
 printf 'Waiting for iroha and grpc-server become ready'
-until $(curl --output /dev/null -m 5 --silent --head --fail http://grpcwebproxy:8080); do
-    printf '.'
-    sleep 5
+while [[ "$(curl -s -o /dev/null -m 5 -w ''%{http_code}'' grpcwebproxy:8080)" != "500" ]]; do 
+  printf '.';
+  sleep 5; 
 done
 
 NODE_IP=http://grpcwebproxy:8080 DEBUG=iroha-util node scripts/setup.js
