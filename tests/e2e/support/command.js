baf50d65530f1b88d3049749f98c7742555560eb
Robert <robert@MacBook-Pro-Robert.local>
2018-08-31 11:46:23 +0300

    test: fixed tests due to white list select field

--- tests/e2e/support/command.js
+++ tests/e2e/support/command.js
@@ -15,10 +15,8 @@ Cypress.Commands.add('upload_file', (fileName, selector) => {
 
 Cypress.Commands.add('login', (keyPath) => {
   cy.get('form > div:nth-child(1) input')
-    .type('test@notary').should('have.value', 'test@notary')
+    .type(keyPath).should('have.value', keyPath)
   cy.upload_file(keyPath, 'input.el-upload__input')
-  cy.get('form > div:nth-child(2) input')
-    .should('have.value', '0f0ce16d2afbb8eca23c7d8c2724f0c257a800ee2bbd54688cec6b898e3f7e33')
   cy.get('form > div:nth-child(3) input')
     .type(Cypress.env('IROHA')).should('have.value', Cypress.env('IROHA'))
   cy.get('.login-button-container > div > button').click()
