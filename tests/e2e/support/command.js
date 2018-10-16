8fb345c59c4703a4b041d05089e34458aa4f4a8d
Robert Sayakhov <roobertdantes@gmail.com>
2018-10-16 17:46:31 +0300

    D3-463 | Change input of nodeIp [Develop] (#81)
    * feat: add nodes and notaryIPs
    
    * feat: selector for login page
    
    * feat: selector for signup page
    
    * test: fixed test
    
    * chore: return back settlements to mocks folder

--- tests/e2e/support/command.js
+++ tests/e2e/support/command.js
@@ -23,6 +23,7 @@ Cypress.Commands.add('login', (keyPath) => {
   cy.upload_file(keyPath, 'input.el-upload__input')
   cy.get('form > div:nth-child(3) input')
     .type(Cypress.env('IROHA')).should('have.value', Cypress.env('IROHA'))
+  cy.get('.el-scrollbar__view > :nth-child(1)').click()
   cy.get('.login-button-container > div > button').click()
   cy.url().should('be.not.eq', `${Cypress.config('baseUrl')}/#/login`)
 })
