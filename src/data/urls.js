6e5b516a11a30902a5cfa2fac6afaf6ab3ef4e0b
asmadek <alma2610@ya.ru>
2018-12-29 15:15:52 +0500

    fix: change logic of getting list of nodes

--- src/data/urls.js
+++ src/data/urls.js
@@ -1,6 +1,7 @@
+import listOfNodes from './nodes.json'
 export const ETH_NOTARY_URL = process.env.VUE_APP_ETH_NOTARY_URL || 'http://localhost:8083'
 export const BTC_NOTARY_URL = process.env.VUE_APP_BTC_NOTARY_URL || 'http://localhost:8084'
-export const IROHA_URL = process.env.VUE_APP_IROHA_URL || 'http://localhost:8081'
+export const IROHA_URL = process.env.VUE_APP_IROHA_URL
 
 export const registrationIPs = [
   {
@@ -13,9 +14,11 @@ export const registrationIPs = [
   }
 ]
 
-export const listOfNodes = [
-  {
+if (IROHA_URL) {
+  listOfNodes.unshift({
     'value': IROHA_URL,
-    'label': 'Node 1'
-  }
-]
+    'label': 'Default Node'
+  })
+}
+
+export { listOfNodes }
