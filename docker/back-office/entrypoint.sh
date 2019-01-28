1b5c7b4d5007bb9b19e03974890883aecd7729fb
Robert Sayakhov <roobertdantes@gmail.com>
2019-01-28 19:25:26 +0300

    chore: update entrypoint

--- docker/back-office/entrypoint.sh
+++ docker/back-office/entrypoint.sh
@@ -4,10 +4,10 @@ yarn
 yarn build
 
 printf 'Waiting for iroha and grpc-server become ready'
-while [[ "$(curl -s -o /dev/null -m 5 -w ''%{http_code}'' grpcwebproxy:8080)" != "500" ]]; do 
+while [[ "$(curl -s -o /dev/null -m 5 -w ''%{http_code}'' grpcwebproxy:8081)" != "500" ]]; do 
   printf '.';
   sleep 5; 
 done
 
-NODE_IP=http://grpcwebproxy:8080 DEBUG=iroha-util node scripts/setup.js
+NODE_IP=http://iroha:50051 DEBUG=iroha-util node scripts/setup.js
 http-server --cors ./dist
