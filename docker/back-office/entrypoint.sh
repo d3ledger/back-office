c13390542e10703860edfbbb2cde39532b7ae0b6
Robert Sayakhov <roobertdantes@gmail.com>
2019-01-29 11:44:07 +0300

    fix: grpc port

--- docker/back-office/entrypoint.sh
+++ docker/back-office/entrypoint.sh
@@ -4,7 +4,7 @@ yarn
 yarn build
 
 printf 'Waiting for iroha and grpc-server become ready'
-while [[ "$(curl -s -o /dev/null -m 5 -w ''%{http_code}'' grpcwebproxy:8081)" != "500" ]]; do 
+while [[ "$(curl -s -o /dev/null -m 5 -w ''%{http_code}'' grpcwebproxy:8080)" != "500" ]]; do 
   printf '.';
   sleep 5; 
 done
