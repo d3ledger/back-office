5077ff427a6b2646d33d0c53c3926556f49fb3f6
Robert Sayakhov <roobertdantes@gmail.com>
2019-01-30 17:56:29 +0300

    D3-776 | Update to RC2 (#118)
    * feat: updated iroha-helpers, docker version and genesis block
    
    * feat: migrate queries to new iroha lib
    
    * chore: update to new version of iroha lib
    
    * feat: update commands, functions, util
    
    * fix: added missed argument
    
    * chore: update iroha-library
    
    * chore: update version of iroha-library
    
    * feat: added function for command and query services
    
    * feat: query for raw transactions
    
    * feat: updated several command functions
    
    * fix: return to raw transactions
    
    * feat: fixed script for setup tokens
    
    * feat: show first 100 transactions
    
    * chore: added d3 to genesis for tests
    
    * feat: updated unit test due to new transactions response
    
    * test: remove console.log
    
    * chore: remove alice private key
    
    * chore: fix rebase issue
    
    * chore: update docker compoe to new version of iroha
    
    * chore: update entrypoint
    
    * chore: add ports to iroha
    
    * chore: remove http protocol
    
    * fix: grpc port
    
    * chore: add sleep timer
    
    * fix:stop after setup script

--- src/util/iroha/commands.js
+++ src/util/iroha/commands.js
@@ -1,50 +1,23 @@
-import { txHelper } from 'iroha-helpers'
-import { CommandServiceClient } from 'iroha-helpers/lib/proto/endpoint_pb_service'
-import { TxStatus, TxStatusRequest } from 'iroha-helpers/lib/proto/endpoint_pb'
 import {
-  cache,
-  getProtoEnumName,
-  DEFAULT_TIMEOUT_LIMIT
+  commands,
+  signWithArrayOfKeys,
+  sendTransactions,
+  txHelper
+} from 'iroha-helpers'
+import {
+  newCommandService,
+  newCommandServiceOptions
 } from './util'
 import cloneDeep from 'lodash/fp/cloneDeep'
 
-import Debug from 'debug'
-const debug = Debug('iroha-util')
+const DEFAULT_TIMEOUT_LIMIT = 5000
 
-/**
- * wrapper function of commands
- * @param {Array.<String>} privateKeys array of private keys
- * @param {Object} tx transaction
- * @param {Number} timeoutLimit
- */
-function command (
-  privateKeys = [''],
-  tx,
-  quorum = 1,
-  timeoutLimit = DEFAULT_TIMEOUT_LIMIT
-) {
-  let txToSend = txHelper.addMeta(tx, {
-    creatorAccountId: cache.username,
-    quorum
-  })
-
-  txToSend = signWithArrayOfKeys(txToSend, privateKeys)
-
-  let txClient = new CommandServiceClient(
-    cache.nodeIp
-  )
-
-  return sendTransactions([txToSend], txClient, timeoutLimit)
-}
-
-/**
- * signPendingTransaction: sign transaction with more keys and send.
- * @param {Array.<String>} privateKeys
- * @param {Object} transaction
- */
+// /**
+//  * signPendingTransaction: sign transaction with more keys and send.
+//  * @param {Array.<String>} privateKeys
+//  * @param {Object} transaction
+//  */
 function signPendingTransaction (privateKeys = [], transaction, timeoutLimit = DEFAULT_TIMEOUT_LIMIT) {
-  debug('starting signPendingTransaction...')
-
   /*
     * TODO: Remove clearSignaturesList() after
     * https://soramitsu.atlassian.net/browse/IR-1680 is completed
@@ -56,162 +29,9 @@ function signPendingTransaction (privateKeys = [], transaction, timeoutLimit = D
 
   txToSend = signWithArrayOfKeys(txToSend, privateKeys)
 
-  let txClient = new CommandServiceClient(
-    cache.nodeIp
-  )
+  let txClient = newCommandService()
 
   return sendTransactions([txToSend], txClient, timeoutLimit)
-}
-
-/**
- * createAccount
- * {@link https://iroha.readthedocs.io/en/latest/api/commands.html#cresate-account createAccount - Iroha docs}
- * @param {Array.<String>} privateKeys
- * @param {String} accountName
- * @param {String} domainId
- * @param {String} publicKey
- * @param {Number} accountQuorum
- */
-function createAccount (privateKeys, accountName, domainId, publicKey, accountQuorum) {
-  debug('starting createAccount...')
-
-  return command(
-    privateKeys,
-    txHelper.addCommand(txHelper.emptyTransaction(), 'createAccount', { accountName, domainId, publicKey }),
-    accountQuorum
-  )
-}
-
-/**
- * createAsset
- * {@link https://iroha.readthedocs.io/en/latest/api/commands.html#create-asset createAsset - Iroha docs}
- * @param {Array.<String>} privateKeys
- * @param {String} assetName
- * @param {String} domainI
- * @param {Number} precision
- * @param {Number} accountQuorum
- */
-function createAsset (privateKeys, assetName, domainId, precision, accountQuorum) {
-  debug('starting createAsset...')
-
-  return command(
-    privateKeys,
-    txHelper.addCommand(txHelper.emptyTransaction(), 'createAsset', { assetName, domainId, precision }),
-    accountQuorum
-  )
-}
-
-/**
- * addAssetQuantity
- * {@link https://iroha.readthedocs.io/en/latest/api/commands.html#add-asset-quantity addAssetQuantity - Iroha docs}
- * @param {Array.<String>} privateKeys
- * @param {String} accountId
- * @param {String} assetId
- * @param {String} amount
- * @param {Number} accountQuorum
- */
-function addAssetQuantity (privateKeys, assetId, amount, accountQuorum) {
-  debug('starting addAssetQuantity...')
-
-  return command(
-    privateKeys,
-    txHelper.addCommand(txHelper.emptyTransaction(), 'addAssetQuantity', { assetId, amount }),
-    accountQuorum
-  )
-}
-
-/**
- * setAccountDetail
- * {@link https://iroha.readthedocs.io/en/latest/api/commands.html#set-account-detail setAccountDetail - Iroha docs}
- * @param {Array.<String>} privateKeys
- * @param {String} accountId
- * @param {String} key
- * @param {String} value
- * @param {Number} accountQuorum
- */
-function setAccountDetail (privateKeys, accountId, key, value, accountQuorum) {
-  debug('starting setAccountDetail...')
-
-  return command(
-    privateKeys,
-    txHelper.addCommand(txHelper.emptyTransaction(), 'setAccountDetail', { accountId, key, value }),
-    accountQuorum
-  )
-}
-
-/**
- * setAccountQuorum
- * {@link https://iroha.readthedocs.io/en/latest/api/commands.html#set-account-quorum setAccountQuorum - Iroha docs}
- * @param {Array.<String>} privateKeys
- * @param {String} accountId
- * @param {Number} quorum
- * @param {Number} accountQuorum
- */
-function setAccountQuorum (privateKeys, accountId, quorum, accountQuorum) {
-  debug('starting setAccountQuorum...')
-
-  return command(
-    privateKeys,
-    txHelper.addCommand(txHelper.emptyTransaction(), 'setAccountQuorum', { accountId, quorum }),
-    accountQuorum
-  )
-}
-
-/**
- * transferAsset
- * {@link https://iroha.readthedocs.io/en/latest/api/commands.html#transfer-asset transferAsset - Iroha docs}
- * @param {Array.<Object>} privateKeys
- * @param {String} srcAccountId
- * @param {String} destAccountId
- * @param {String} assetId
- * @param {String} description
- * @param {String} amount
- * @param {Number} accountQuorum
- */
-function transferAsset (privateKeys, srcAccountId, destAccountId, assetId, description, amount, accountQuorum) {
-  debug('starting transferAsset...')
-
-  return command(
-    privateKeys,
-    txHelper.addCommand(txHelper.emptyTransaction(), 'transferAsset', { srcAccountId, destAccountId, assetId, description, amount }),
-    accountQuorum
-  )
-}
-
-/**
- * addSignatory
- * {@link https://iroha.readthedocs.io/en/latest/api/commands.html#add-signatory addSignatory - Iroha docs}
- * @param {Array.<String>} privateKeys
- * @param {String} accountId
- * @param {Buffer} publicKey
- * @param {Number} accountQuorum
- */
-function addSignatory (privateKeys, accountId, publicKey, accountQuorum) {
-  debug('starting addSignatory...')
-
-  return command(
-    privateKeys,
-    txHelper.addCommand(txHelper.emptyTransaction(), 'addSignatory', { accountId, publicKey }),
-    accountQuorum
-  )
-}
-
-/**
- * removeSignatory
- * {@link https://iroha.readthedocs.io/en/latest/api/commands.html#remove-signatory removeSignatory - Iroha docs}
- * @param {Array.<String>} privateKeys
- * @param {String} accountId
- * @param {Buffer} publicKey
- * @param {Number} accountQuorum
- */
-function removeSignatory (privateKeys, accountId, publicKey, accountQuorum) {
-  debug('starting removeSignatory...')
-
-  return command(
-    privateKeys,
-    txHelper.addCommand(txHelper.emptyTransaction(), 'removeSignatory', { accountId, publicKey }),
-    accountQuorum
-  )
 }
 
 /**
@@ -228,12 +48,8 @@ function removeSignatory (privateKeys, accountId, publicKey, accountQuorum) {
  * @param {String} receiverAmount
  * @param {Number} timeoutLimit
  */
-function createSettlement (senderPrivateKeys, senderAccountId = cache.username, senderQuorum = 1, senderAssetId, senderAmount, description, receiverAccountId, receiverQuorum = 1, receiverAssetId, receiverAmount, timeoutLimit = DEFAULT_TIMEOUT_LIMIT) {
-  debug('starting createSettlement...')
-
-  let txClient = new CommandServiceClient(
-    cache.nodeIp
-  )
+function createSettlement (senderPrivateKeys, senderAccountId, senderQuorum = 1, senderAssetId, senderAmount, description, receiverAccountId, receiverQuorum = 1, receiverAssetId, receiverAmount, timeoutLimit = DEFAULT_TIMEOUT_LIMIT) {
+  let txClient = newCommandService()
 
   let senderTx = txHelper.addCommand(txHelper.emptyTransaction(), 'transferAsset', { srcAccountId: senderAccountId, destAccountId: receiverAccountId, assetId: senderAssetId, description, amount: senderAmount })
   senderTx = txHelper.addMeta(senderTx, { creatorAccountId: senderAccountId, senderQuorum })
@@ -248,12 +64,9 @@ function createSettlement (senderPrivateKeys, senderAccountId = cache.username, 
 }
 
 function acceptSettlement (privateKeys, batchArray, timeoutLimit = DEFAULT_TIMEOUT_LIMIT) {
-  debug('starting acceptSettlement')
   if (!batchArray.length) return
 
-  let txClient = new CommandServiceClient(
-    cache.nodeIp
-  )
+  let txClient = newCommandService()
 
   const indexOfUnsigned = cloneDeep(batchArray)
     .map(tx => tx.toObject())
@@ -269,12 +82,9 @@ function acceptSettlement (privateKeys, batchArray, timeoutLimit = DEFAULT_TIMEO
 }
 
 function rejectSettlement (privateKeys, batchArray, timeoutLimit = DEFAULT_TIMEOUT_LIMIT) {
-  debug('starting acceptSettlement')
   if (!batchArray.length) return
 
-  let txClient = new CommandServiceClient(
-    cache.nodeIp
-  )
+  let txClient = newCommandService()
 
   const indexOfUnsigned = cloneDeep(batchArray)
     .map(tx => tx.toObject())
@@ -292,91 +102,97 @@ function rejectSettlement (privateKeys, batchArray, timeoutLimit = DEFAULT_TIMEO
   ])
 }
 
-/**
- * Send transaction: used for sending transactions to iroha
- * @param {Array.<Objects>} txs
- * @param {Object} txClient
- * @param {Number} timeoutLimit - timeout limit of the transaction
- * @param {Array.<String>} requiredStatuses - list of required statuses of the response
- */
-function sendTransactions (txs, txClient, timeoutLimit, requiredStatuses = [
-  'MST_PENDING',
-  'COMMITTED'
-]) {
-  const hashes = txs.map(x => txHelper.hash(x))
-  const txList = txHelper.createTxListFromArray(txs)
+const createAccount = (privateKeys, quorum, {
+  accountName,
+  domainId,
+  publicKey
+}) => commands.createAccount(
+  newCommandServiceOptions(privateKeys, quorum), {
+    accountName,
+    domainId,
+    publicKey
+  }
+)
 
-  return new Promise((resolve, reject) => {
-    debug('submitting transactions...')
-    debug('peer ip:', cache.nodeIp)
-    txs.forEach(x =>
-      debug('parameters sender:', JSON.stringify(x.getPayload(), null, '  '))
-    )
-    hashes.forEach(x =>
-      debug('receiverTxHash:', Buffer.from(x).toString('hex'))
-    )
-    debug('')
+const createAsset = (privateKeys, quorum, {
+  assetName,
+  domainId,
+  precision
+}) => commands.createAsset(
+  newCommandServiceOptions(privateKeys, quorum), {
+    assetName,
+    domainId,
+    precision
+  }
+)
 
-    const timer = setTimeout(() => {
-      txClient.$channel.close()
-      reject(new Error('please check IP address OR your internet connection'))
-    }, timeoutLimit)
+const transferAsset = (privateKeys, quorum, {
+  srcAccountId,
+  destAccountId,
+  assetId,
+  description,
+  amount
+}) => commands.transferAsset(
+  newCommandServiceOptions(privateKeys, quorum), {
+    srcAccountId,
+    destAccountId,
+    assetId,
+    description,
+    amount
+  }
+)
 
-    // Sending even 1 transaction to listTorii is absolutely ok and valid.
-    txClient.listTorii(txList, (err, data) => {
-      clearTimeout(timer)
+const addSignatory = (privateKeys, quorum, {
+  accountId,
+  publicKey
+}) => commands.addSignatory(
+  newCommandServiceOptions(privateKeys, quorum), {
+    accountId,
+    publicKey
+  }
+)
 
-      if (err) {
-        return reject(err)
-      }
+const removeSignatory = (privateKeys, quorum, {
+  accountId,
+  publicKey
+}) => commands.removeSignatory(
+  newCommandServiceOptions(privateKeys, quorum), {
+    accountId,
+    publicKey
+  }
+)
 
-      debug('submitted transaction successfully!')
-      resolve()
-    })
-  })
-    .then(() => {
-      return new Promise((resolve, reject) => {
-        debug('opening transaction status stream...')
+const addAssetQuantity = (privateKeys, quorum, {
+  assetId,
+  amount
+}) => commands.addAssetQuantity(
+  newCommandServiceOptions(privateKeys, quorum), {
+    assetId,
+    amount
+  }
+)
 
-        // Status requests promises
-        let requests = hashes.map(hash => new Promise((resolve, reject) => {
-          let statuses = []
+const setAccountDetail = (privateKeys, quorum, {
+  accountId,
+  key,
+  value
+}) => commands.setAccountDetail(
+  newCommandServiceOptions(privateKeys, quorum), {
+    accountId,
+    key,
+    value
+  }
+)
 
-          let request = new TxStatusRequest()
-          request.setTxHash(hash)
-
-          let stream = txClient.statusStream(request)
-          stream.on('data', function (response) {
-            statuses.push(response)
-          })
-
-          stream.on('end', function (end) {
-            statuses.length > 0 ? resolve(statuses[statuses.length - 1].getTxStatus()) : resolve(null)
-          })
-        }))
-
-        Promise.all(requests).then(values => {
-          let statuses = values.map(x => x !== null ? getProtoEnumName(
-            TxStatus,
-            'iroha.protocol.TxStatus',
-            x
-          ) : null)
-          statuses.some(x => requiredStatuses.includes(x))
-            ? resolve()
-            : reject(
-              new Error(`Your transaction wasn't commited: expected: ${requiredStatuses}, actual=${statuses}`)
-            )
-        })
-      })
-    })
-}
-
-function signWithArrayOfKeys (tx, privateKeys) {
-  privateKeys.forEach(key => {
-    tx = txHelper.sign(tx, key)
-  })
-  return tx
-}
+const setAccountQuorum = (privateKeys, currentQuorum, {
+  accountId,
+  quorum
+}) => commands.setAccountQuorum(
+  newCommandServiceOptions(privateKeys, currentQuorum), {
+    accountId,
+    quorum
+  }
+)
 
 export {
   createAccount,
