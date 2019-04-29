7d1e3419dc8a55ff05ec0427b7f7def8c3b002d5
Pobepto <roobertdantes@gmail.com>
2019-04-29 13:12:50 +0300

    fix: bug with fourth active tab

--- src/components/Settings/components/WalletLimitsCard.vue
+++ src/components/Settings/components/WalletLimitsCard.vue
@@ -18,7 +18,7 @@
           <fa-icon class="action_button-icon" icon="plus" /> Add
         </el-button>
       </div>
-      <div v-if="activeTab === 3">
+      <div v-if="activeTab === 4">
         <div class="settings-item">
           <template v-for="(limit, index) in accountLimits">
             <div
