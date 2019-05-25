9f08bd114298e84b06e7ff23107d9a0499093858
Pobepto <roobertdantes@gmail.com>
2019-05-25 15:12:30 +0300

    feat: added util for requesting data collector

---
+++ src/util/collector-util.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+// TODO: Need to create file where we can store such variables
+const PROTOCOL = location.protocol
+
+const getAllAssets = (url) => {
+  console.log(url)
+  return axios({
+    url: '/iroha/asset/getAll',
+    baseURL: `${PROTOCOL}//${url}`
+  })
+    .then(({ data }) => data)
+    .catch(err => console.error(err))
+}
+
+export default {
+  getAllAssets
+}
