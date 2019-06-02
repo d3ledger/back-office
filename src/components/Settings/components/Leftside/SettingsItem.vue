419827824a038a22732496a87875669aa1fb806b
Pobepto <roobertdantes@gmail.com>
2019-06-02 13:40:52 +0300

    feat: created settings item component

---
+++ src/components/Settings/components/Leftside/SettingsItem.vue
@@ -0,0 +1,39 @@
+<template>
+  <el-row class="settings_item">
+    <div class="settings_item-header">
+      <span class="settings_item-header-title">
+        {{ title }}
+      </span>
+    </div>
+    <div>
+      <slot />
+    </div>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: 'SettingsItem',
+  props: {
+    title: {
+      type: String,
+      required: true,
+      default: 'Settings Item'
+    }
+  }
+}
+</script>
+
+<style scope>
+.settings_item {
+  margin-bottom: 20px;
+}
+.settings_item-header {
+  margin-bottom: 15px;
+}
+
+.settings_item-header-title {
+  font-size: 1rem;
+  font-weight: 300;
+}
+</style>
