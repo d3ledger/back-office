ed7f4603026e8f9002624ec25acf831aa6b960ac
Pobepto <roobertdantes@gmail.com>
2019-08-15 15:45:31 +0300

    test: update username with domain

--- tests/e2e/integration/auth.spec.js
+++ tests/e2e/integration/auth.spec.js
@@ -21,8 +21,8 @@ describe('Test register page', () => {
   // Signing up should fail because the registration service doesn't work on CI now
   it('Register new user - failure', () => {
     cy.get('.el-input__inner[name="username"]')
-      .type('jasonstatham')
-      .should('have.value', 'jasonstatham')
+      .type('jason@statham')
+      .should('have.value', 'jason@statham')
     cy.get('.el-input__inner[name="publicKey"]')
       .type('9c430dfe8c54b0a447e25f75121119ac3b649c1253bce8eeeeeeeeeeeeeeeeee')
       .should('have.value', '9c430dfe8c54b0a447e25f75121119ac3b649c1253bce8eeeeeeeeeeeeeeeeee')
